345eccd7b2a20c23e33856572d74a86a2cfcdefd
Gautham P <gauthampdn@gmail.com>
2023-03-11 00:00:58 -0800

    all changes

--- agws23/src/App.js
+++ agws23/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Studyplan from "./Components/Studyplan";
+
+const { Configuration, OpenAIApi } = require("openai");
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const configuration = new Configuration({
+    // apiKey: "API HERE",
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  const [option, setOption] = useState({});
+  const [input, setInput] = useState("");
+
+  const givePlan = async () => {
+    const openaitext = {
+      model: "text-davinci-003",
+      temperature: 0.8,
+      max_tokens: 60,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+      stop: ["\n"],
+      prompt: input,
+    };
+
+    const response = await openai.createCompletion(openaitext);
+
+    console.log(openaitext);
+    console.log(response);
+  };
+
+  return <Studyplan givePlan={givePlan} setInput={setInput} />;
 }
 
 export default App;
