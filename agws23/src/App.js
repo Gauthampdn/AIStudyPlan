04ac062bc921b9418e4cdb234dee68ebf4822119
Gautham P <gauthampdn@gmail.com>
2023-03-11 02:13:32 -0800

    edit

--- agws23/src/App.js
+++ agws23/src/App.js
@@ -1,38 +1,48 @@
 import { useState } from "react";
-import "./App.css";
-import Studyplan from "./Components/Studyplan";
+import Form from "./Form";
+import './App.css';
 
 const { Configuration, OpenAIApi } = require("openai");
 
+const configuration = new Configuration({
+  apiKey: "API HERE",
+});
+
+const openai = new OpenAIApi(configuration);
+
 function App() {
-  const configuration = new Configuration({
-    // apiKey: "API HERE",
-  });
+  const [studyPlan, setStudyPlan] = useState(null);
+  const [responseText, setResponseText] = useState("");
 
-  const openai = new OpenAIApi(configuration);
+  const handleFormSubmit = async (plan) => {
+    setStudyPlan(plan);
+    console.log(plan);
 
-  const [option, setOption] = useState({});
-  const [input, setInput] = useState("");
-
-  const givePlan = async () => {
-    const openaitext = {
+    const response = await openai.createCompletion({
       model: "text-davinci-003",
-      temperature: 0.8,
-      max_tokens: 60,
+      prompt: plan,
+      temperature: 0.5,
+      max_tokens: 200,
       top_p: 1.0,
       frequency_penalty: 0.0,
       presence_penalty: 0.0,
-      stop: ["\n"],
-      prompt: input,
-    };
+    });
 
-    const response = await openai.createCompletion(openaitext);
-
-    console.log(openaitext);
-    console.log(response);
+    console.log(response.data.choices[0].text);
+    setResponseText(response.data.choices[0].text);
   };
 
-  return <Studyplan givePlan={givePlan} setInput={setInput} />;
+  return (
+    <div>
+      <h1>The Ultimate Plan Maker</h1>
+      <h4>Meet Otto, a brilliant plan maker with sharp intellect and unwavering focus. His precise and efficient strategies earn him a true genius reputation.</h4>
+      <Form onFormSubmit={handleFormSubmit} />
+      <p dangerouslySetInnerHTML={{ __html: responseText }}></p>
+    </div>
+  );
+  
+  
+  
 }
 
 export default App;
